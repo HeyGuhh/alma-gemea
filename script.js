6d915ad48ff1afee52e64df6bd5789746bc9f286
Gustavo <68632988+HeyGuhh@users.noreply.github.com>
2024-09-20 20:22:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,12 +4,30 @@ const cardAreaElement = document.querySelector('.div__recadinhos-content')
 const popUpElement = document.querySelector('.div__popup');
 const divLoveYouElement = document.querySelector('.div__love-you-content');
 const pLoveCountry = document.querySelector('.p__love-country');
+const audioElement = document.querySelector('.audio__music');
+const divMusicElement = document.querySelector('.div__music-content');
 
 const letterTexts = [
     '<p><strong>Para o meu amor</strong><br /><br />Eu disse e não me arrependo de dizer novamente, você foi, é, e sempre será minha melhor escolha.</p>',
     '<p><strong>Para a pessoa mais linda deste mundo</strong><br /><br />Eu amava tanto o azul das águas, dos mares e do céu... mas eles perderam o encanto assim que conheci o castanho dos seus olhos.</p>',
     '<p><strong>Para o amor da minha vida</strong><br /><br />Eu sempre achei que amar era algo tão ruim. Mas quando te conheci, você me mostrou que amar é um dos sentimentos mais puros e lindos do mundo inteiro.</p>',
-    '<p><strong>Para a pessoa mais importante para mim</strong><br /><br />Às vezes eu exagero e eu sei disso, mas não me imagino tendo uma vida na qual você não participa, você sempre está presente nos meus sonhos e planos do futuro.</p>',
+    '<p><strong>Para o meu motivo de viver</strong><br /><br />Às vezes eu exagero e eu sei disso, mas não me imagino tendo uma vida na qual você não participa, você sempre está presente nos meus sonhos e planos do futuro.</p>',
+    '<p><strong>Para a pessoa mais importante para mim</strong><br /><br />Você me fez descobrir o meu melhor lado, despertou o melhor de mim e vai despertando cada vez mais com apenas um sorriso.</p>'
+];
+
+const musics = [
+    { letter: 'Essa música vem na minha cabeça sempre que penso em ti.', src: './sounds/vem ser minha.mp3' }, 
+    { letter: 'Essa música toca na minha cabeça sempre que te vejo. ', src: './sounds/lisboa.mp3' }, 
+    { letter: 'Caso se sinta sozinha, lembre-se:', src: './sounds/photograph.mp3' }, 
+    { letter: 'Essa música me lembra nosso segundo beijo, porque o primeiro vish...', src: './sounds/to com moral no ceu.mp3' },
+    { letter: 'Se algum dia duvidar do meu amor por ti, escute isso:', src: './sounds/perfect.mp3' }, 
+    { letter: 'Se algum dia duvidar de que a unica pessoa que eu quero é você, ouça:', src: './sounds/nothing on you.mp3' },  
+    { letter: 'Se eu te acho linda?', src: './sounds/just the way.mp3' },  
+    { letter: 'O que eu quero que você seja?', src: './sounds/nosso lugar.mp3' }, 
+    { letter: 'Nossa musica.', src: './sounds/sagrado profano.mp3' },  
+    { letter: 'O que eu vou fazer por você?', src: './sounds/a droga do amor.mp3' }, 
+    { letter: 'Minhas intenções com você?', src: './sounds/i wanna be yours.mp3' },   
+    { letter: 'Estar com você me faz sentir assim', src: './sounds/terça de tarde.mp3' },
 ];
 
 /** Retorna quanto tempo passou */
@@ -193,24 +211,128 @@ function loveYouText() {
 }
 
 setInterval(() => {
-    const [, next, ,current, ,last] = divLoveYouElement.childNodes;
+    const [, next, , current, , last] = divLoveYouElement.childNodes;
     const randomText = loveYouText();
     next.innerText = `${randomText.message}`;
     pLoveCountry.innerText = randomText.country;
 
     // Default
-    next.animate({ transform: "translate(50%, -50%)", opacity: 0}, { duration: 0, fill: 'forwards'})
-    last.animate({ transform: "translate(50%, 50%)", opacity: 1}, { duration: 0, fill: 'forwards'})
-    
+    next.animate({ transform: "translate(50%, -50%)", opacity: 0 }, { duration: 0, fill: 'forwards' })
+    last.animate({ transform: "translate(50%, 50%)", opacity: 1 }, { duration: 0, fill: 'forwards' })
+
     // Animating
-    next.animate({ transform: "translate(50%, 50%)", opacity: 1}, { duration: 400, fill: 'forwards'})
-    last.animate({ transform: "translate(50%, 100%)", opacity: 0}, { duration: 400, fill: 'forwards'})
-    
+    next.animate({ transform: "translate(50%, 50%)", opacity: 1 }, { duration: 400, fill: 'forwards' })
+    last.animate({ transform: "translate(50%, 100%)", opacity: 0 }, { duration: 400, fill: 'forwards' })
+
     // Default
     setTimeout(() => {
-        next.animate({ transform: "translate(50%, -50%)", opacity: 0}, { duration: 0, fill: 'forwards'})
-        last.animate({ transform: "translate(50%, 50%)", opacity: 1}, { duration: 0, fill: 'forwards'})
+        next.animate({ transform: "translate(50%, -50%)", opacity: 0 }, { duration: 0, fill: 'forwards' })
+        last.animate({ transform: "translate(50%, 50%)", opacity: 1 }, { duration: 0, fill: 'forwards' })
 
         last.innerText = next.innerHTML;
     }, 1000);
 }, 2000);
+
+// configurando sons 
+let divMusicsElements = [];
+function setSoundElements(count) {
+    const secureArray = [...musics];
+    const biggestValue = secureArray.sort((a, b) => b.letter.length - a.letter.length)[0];
+    const biggestIndex = musics.findIndex(f => f.src === biggestValue.src);
+
+    for (let countX = 0; countX < count; countX++) {
+        const divElement = document.createElement('div');
+        const letterElement = document.createElement('p'); 
+        const iconElement = document.createElement('p');
+
+        if (countX === biggestIndex) {
+            divElement.setAttribute('class', 'div__music-area div__music-area-base div__music-area-pre');
+            letterElement.innerText = biggestValue.letter;
+            letterElement.classList.add('p__music-letter-pre');
+        }
+        else 
+            divElement.setAttribute('class', 'div__music-area');
+        
+        iconElement.innerText = '▶';
+        letterElement.classList.add('p__music-letter');
+        iconElement.classList.add('p__music-icon');
+
+        divElement.appendChild(letterElement);
+        divElement.appendChild(iconElement);
+        divMusicElement.appendChild(divElement);
+        divMusicsElements.push(divElement); 
+        iconElement.onclick = () => showMusic(countX);
+    }
+
+}
+
+setSoundElements(musics.length);
+
+// Configurando altura padrão
+function configureHeight() {
+    const elementBase = document.querySelector('.div__music-area-base');
+    console.log(elementBase.childNodes);
+    let musicHeight = elementBase.clientHeight;
+    divMusicsElements.forEach(e => e.style.height = `${musicHeight}px`);
+    elementBase.classList.remove('div__music-area-pre');
+    elementBase.childNodes[0].classList.remove('p__music-letter-pre');  
+}
+
+configureHeight();
+
+// Configurando caixa de músicas
+let lastSongElement = { letter: '', div: '' };
+function showMusic(index) {
+    const element = divMusicsElements[index];
+    const text = element.childNodes[0];
+    const icon = element.childNodes[1];
+
+    // Abrir
+    if (!text.classList.contains('p__music-letter-start')) {
+        closeAll();
+
+        text.classList.add('p__music-letter-start');
+        element.classList.add('div__music-area-start');
+        icon.classList.add('p__music-icon-start');
+
+        icon.innerText = '❚❚';
+        lastSongElement = { letter: text, div: element, icon };
+        startSound(index, text); 
+        fadeInSound();
+    } 
+
+    else {
+        text.classList.remove('p__music-letter-start');
+        element.classList.remove('div__music-area-start');
+        icon.classList.remove('p__music-icon-start');
+        
+        audioElement.pause();
+        audioElement.currentTime = 0; 
+        icon.innerText = '▶';
+    }
+
+    async function closeAll() { 
+        lastSongElement.letter.classList?.remove('p__music-letter-start');
+        lastSongElement.div.classList?.remove('div__music-area-start');
+        if (!lastSongElement.icon) return;
+        lastSongElement.icon.innerText = '▶'; 
+        lastSongElement.icon.classList.remove('p__music-icon-start');
+    }
+
+    function delay(ms) { return new Promise(res => setTimeout(res, ms))};
+
+    async function fadeInSound() {
+        audioElement.volume = 0;
+        for (let countX = 0; countX < 1; countX += 0.1) {
+            audioElement.volume = countX;
+            await delay(100);
+        }
+    }
+}
+
+function startSound(index, text) {
+    const music = musics[index];
+    audioElement.setAttribute('src', music.src);
+    text.innerText = music.letter;
+    audioElement.play();
+}
